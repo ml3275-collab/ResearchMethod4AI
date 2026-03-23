0e2758be8e56b65e72461f2631093552ef78becb
Matthew Landon <mlandon@haverford.edu>
2026-03-23 14:47:33 -0400

    A3

--- assignments/src/metrics.py
+++ assignments/src/metrics.py
@@ -3,39 +3,42 @@ import numpy as np
 class Metrics:
     def __init__(self, y, y_pred):
         # y and y_pred are 1-d arrays of true values and predicted values
-        self.y = y
-        self.y_pred = y_pred
+        self.y = np.array(y)
+        self.y_pred = np.array(y_pred)
 
     def acc(self):
         # Accuracy
-        return 1.0-np.sum(np.abs(np.array(self.y) - np.array(self.y_pred)))/len(self.y)
+        return 1.0 - np.sum(np.abs(self.y - self.y_pred)) / len(self.y)
 
     def eod(self, s):
         # Equal Opportunity Difference
-        # s: list/array of binary values 1/0, sensitive attribute
-        # return: EOD = TPR(s=1)-TPR(s=0)
-        # TPR = #(y=1, y_pred=1) / #(y=1)
-        # Write your code below:
+        s = np.array(s)
+        mask1, mask0 = s == 1, s == 0
 
-        return tpr1-tpr0
+        tpr1 = np.sum((self.y[mask1] == 1) & (self.y_pred[mask1] == 1)) / np.sum(self.y[mask1] == 1)
+        tpr0 = np.sum((self.y[mask0] == 1) & (self.y_pred[mask0] == 1)) / np.sum(self.y[mask0] == 1)
+
+        return tpr1 - tpr0
 
     def aod(self, s):
         # Average Odds Difference
-        # s: list/array of binary values 1/0, sensitive attribute
-        # return: AOD = (TPR(s=1)-TPR(s=0)+FPR(s=1)-FPR(s=0))/2
-        # TPR = #(y=1, y_pred=1) / #(y=1)
-        # FPR = #(y=0, y_pred=1) / #(y=0)
-        # Write your code below:
+        s = np.array(s)
+        mask1, mask0 = s == 1, s == 0
 
-        return (tpr1 - tpr0 + fpr1 - fpr0)/2.0
+        tpr1 = np.sum((self.y[mask1] == 1) & (self.y_pred[mask1] == 1)) / np.sum(self.y[mask1] == 1)
+        tpr0 = np.sum((self.y[mask0] == 1) & (self.y_pred[mask0] == 1)) / np.sum(self.y[mask0] == 1)
+
+        fpr1 = np.sum((self.y[mask1] == 0) & (self.y_pred[mask1] == 1)) / np.sum(self.y[mask1] == 0)
+        fpr0 = np.sum((self.y[mask0] == 0) & (self.y_pred[mask0] == 1)) / np.sum(self.y[mask0] == 0)
+
+        return (tpr1 - tpr0 + fpr1 - fpr0) / 2.0
 
     def spd(self, s):
         # Statistical Parity Difference
-        # s: list/array of binary values 1/0, sensitive attribute
-        # return: SPD = |PR(s=1)-PR(s=0)|
-        # PR = #(y_pred=1) / #y
-        # Write your code below:
+        s = np.array(s)
+        mask1, mask0 = s == 1, s == 0
+
+        pr1 = np.sum(self.y_pred[mask1] == 1) / np.sum(mask1)
+        pr0 = np.sum(self.y_pred[mask0] == 1) / np.sum(mask0)
 
         return np.abs(pr1 - pr0)
-
-
