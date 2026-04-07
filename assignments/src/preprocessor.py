038a69f9f416f0c92c2ac0d8bd489af4119ca6c7
Matthew Landon <mlandon@haverford.edu>
2026-04-06 23:55:22 -0400

    5

--- assignments/src/preprocessor.py
+++ assignments/src/preprocessor.py
@@ -8,7 +8,19 @@ def Reweighing(X, Y, A):
     # Return: sample_weight, an array of float weight for every data point
     #         sample_weight(a,y) = P(y)*P(a)/P(a,y)
     # Write your code below:
-
+    df = pd.DataFrame({'a': X[A], 'y': Y})
+    n = len(df)
+    
+    sample_weight = np.zeros(n)
+    
+    p_y = df['y'].value_counts(normalize=True).to_dict()
+    p_a = df['a'].value_counts(normalize=True).to_dict()
+    p_ay = (df.groupby(['a', 'y']).size() / n).to_dict()
+    
+    for (val_a, val_y), prob_ay in p_ay.items():
+        weight = (p_y[val_y] * p_a[val_a]) / prob_ay
+        mask = (df['a'] == val_a) & (df['y'] == val_y)
+        sample_weight[mask] = weight
 
     # Rescale the sum of sample weights to len(y) before returning it
     sample_weight = sample_weight * len(Y) / sum(sample_weight)
